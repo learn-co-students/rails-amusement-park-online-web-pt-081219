d5ae800b768ab94d9d187ee9a7f0dc462f29f5d7
James <jamescscalise@gmail.com>
2020-02-10 19:19:11 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :attractions
+  get '/signin' => "sessions#new"
+  post '/signin' => 'sessions#create'
+  root "users#new"
+  get '/logout' => 'sessions#destroy'
+  post '/attractions/:id' => 'users#ride'
 end
