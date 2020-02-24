340eb8a4db66cebe677e41965c3c8273903c225c
Jenna Ransom <jenna.ransom@gmail.com>
2020-02-23 18:06:52 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,36 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height 
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+      "Sorry. You are not tall enough to ride the #{self.attraction.name}." 
+    elsif self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    else 
+      update_happiness
+      update_nausea
+      update_tickets
+      "Thanks for riding the #{self.attraction.name}!"
+    end
+  end 
+
+  private 
+
+  def update_tickets
+    self.user.tickets -= self.attraction.tickets
+    self.user.save
+  end 
+
+  def update_nausea 
+    self.user.nausea += self.attraction.nausea_rating
+    self.user.save
+  end 
+
+  def update_happiness 
+    self.user.happiness += self.attraction.happiness_rating 
+    self.user.save 
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
+  resources :attractions
+  resources :rides, only: [:create]
+   
+  get 'signin' => 'sessions#new'
+  post 'signin' => 'sessions#create'
+  post 'logout' => 'sessions#destroy'
+
+  root 'welcome#home'
 end
